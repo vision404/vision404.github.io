35c9ae1dc4a0b429860a3661d0a69f24864a0509
vision404 <vision34@yeah.net>
2025-07-04 19:43:40 +0800

    Site updated: 2025-07-04 19:43:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/10/09/你好/","2023/10/15/2023年11月12日短讯/","2023/10/09/关于/","2023/10/12/农行常用二维码/"];function toRandomPost(){
+var posts=["2023/10/15/2023年11月12日短讯/","2023/10/09/关于/","2023/10/09/你好/","2023/10/12/农行常用二维码/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
